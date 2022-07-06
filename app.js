7c7e26124da68e2c91ebfb23bb42cf5b44cebd87
ogoiddev <diogompantaleao@gmail.com>
2022-07-06 02:37:32 -0300

    updown

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const swaggerFile = require('./swagger_output.json');
 const productsRouter = require('./routes/productsRouter');
 const salesRouter = require('./routes/salesRouter');
 const error = require('./middleware/errorMiddleware');
+const connection = require('./server/connection');
 
 const app = express();
 
@@ -13,6 +14,19 @@ const app = express();
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/db', async (_req, res) => {
+    try {
+      const client = await connection.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { results: (result) ? result.rows : null };
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send(`Error ${err}`);
+    }
+  });
 
 app.use(bodyParser.json());
 
--- server/connection.js
+++ server/connection.js
@@ -1,11 +1,25 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
-
-  const connection = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE || 'StoreManager',
+const { Pool } = require('pg');
+require('dotenv').config();
+  
+  const connection = new Pool({
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE || 'StoreManager',
+    connectionString: process.env.DATABASE_URL,
+    // ssl: {
+    //   rejectUnauthorized: false,
+    // },
   });
 
 module.exports = connection;
+  
+// const mysql = require('mysql2/promise');
+// require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
+
+//   const connection = mysql.createPool({
+//     host: process.env.MYSQL_HOST,
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_PASSWORD,
+//     database: process.env.MYSQL_DATABASE || 'StoreManager',
+//   });
